c35d9cf1933e0f90ae1c79d5cc09f63a1d0783ec
semihbulut <semihbulut@outlook.com.tr>
2017-12-17 20:46:26 +0300

    Models.py dosyasina kullanici profili icin ozellik eklendi

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import  post_save
 
 # Create your models here.
 
@@ -11,4 +12,10 @@ class KullaniciProfili(models.Model):
     sehir =models.CharField(max_length=100, default='')
     websayfa =models.URLField(default='')
     telefon =models.IntegerField(default=0)
+
+def profil_olustur(sender, **kwargs):
+    if kwargs['created']:
+        user_profile =KullaniciProfili.objects.create(kullanici=kwargs['instance'])
+
+post_save.connect(profil_olustur, sender=User)
 
